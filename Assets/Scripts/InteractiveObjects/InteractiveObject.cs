59424da329e95a1985a1afbbdfa492e9005fedc2
Isaeo22 <isaeo22@gmail.com>
2022-11-19 15:08:47 +0100

    Puzzle2Metido

--- Assets/Scripts/InteractiveObjects/InteractiveObject.cs
+++ Assets/Scripts/InteractiveObjects/InteractiveObject.cs
@@ -13,7 +13,7 @@ public class InteractiveObject : MonoBehaviour
 
    public void ActivarObjeto()//metodo para crear el puzzle por primera vez
    {
-        puzzle.GetComponent<Puzzle1>().IniciarPuzzle();
+        puzzle.GetComponent<PuzzlePadre>().IniciarPuzzle();
         listaCamaras[0].gameObject.SetActive(false);
         listaCamaras[1].gameObject.SetActive(true);
    }
